89d84c5480f195a6cccf0a1b18bdc2279f4606d1
Jujeongmin <anjshdkdl99@gmail.com>
2025-08-26 18:49:02 +0900

    feat: Input board, Sudoku board frame

--- Assets/02.Scripts/Board/Board.cs
+++ Assets/02.Scripts/Board/Board.cs
@@ -1,20 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
     public GameObject cellPrefab;
+    public Transform boardParent;
+    public GameObject numberPanel;
+    public Button[] numberButtons;
 
+    private Cell[,] cells = new Cell[9, 9];
+    private Cell selectedCell;
 
     void Start()
     {
-        
+        GenerateBoard();
+        SetupNumberPanel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenerateBoard()
     {
-        
+        int[,] initialNumbers = new int[9, 9] {
+            {5,0,0,0,0,0,0,1,0},
+            {0,0,0,0,7,0,0,0,0},
+            {0,0,0,0,0,0,6,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,0,0,0,0,5,0,0,0},
+            {0,0,0,0,0,0,0,0,3},
+            {0,0,0,0,4,0,0,0,0},
+            {0,0,2,0,0,0,0,0,0},
+            {0,8,0,0,0,0,0,0,0}
+        };
+
+        for (int r = 0; r < 9; r++)
+        {
+            for (int c = 0; c < 9; c++)
+            {
+                GameObject cellObj = Instantiate(cellPrefab, boardParent);
+                Cell cell = cellObj.GetComponent<Cell>();
+                cell.Init(r, c, OnCellClicked, initialNumbers[r, c]);
+                cells[r, c] = cell;
+            }
+        }
+    }
+
+    public void SetupNumberPanel()
+    {
+        numberPanel.SetActive(false);
+
+        foreach (var btn in numberButtons)
+        {
+            int number = int.Parse(btn.GetComponentInChildren<TMP_Text>().text);
+            btn.onClick.AddListener(() => OnNumberSelected(number));
+        }
+    }
+
+    public void OnCellClicked(Cell cell)
+    {
+        selectedCell = cell;
+        numberPanel.SetActive(true);
+    }
+
+    public void OnNumberSelected(int number)
+    {
+        if (selectedCell != null)
+            selectedCell.SetNumber(number);
+        numberPanel.SetActive(false);
     }
 }
--- Assets/02.Scripts/Board/Cell.cs
+++ Assets/02.Scripts/Board/Cell.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text numberText;
+
+    private Button button;
+    private int row;
+    private int col;
+    private bool isFixed = false;
+    private Action<Cell> onClick;
+
+    public void Init(int r, int c, Action<Cell> clickCallback, int initNumber = 0)
     {
-        
+        row = r;
+        col = c;
+        onClick = clickCallback;
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(() =>
+        {
+            if (!isFixed) onClick(this);
+        });
+
+        if (initNumber != 0)
+        {
+            numberText.text = initNumber.ToString();
+            isFixed = true;
+            numberText.color = Color.gray;
+        }
+        else
+        {
+            numberText.text = "";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetNumber(int number)
     {
-        
+        if (!isFixed)
+        {
+            numberText.text = number.ToString();
+        }
     }
 }
